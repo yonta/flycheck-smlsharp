a5e8b643fd268ceefc3110d765b434a81f776c70
SAITOU Keita <keita44.f4@gmail.com>
2019-04-07 00:42:24 +0900

    Update: Emacs documentation of checker smlsharp
    Some comments is updated to unify words.

--- flycheck-smlsharp.el
+++ flycheck-smlsharp.el
@@ -21,18 +21,42 @@
 (require 'flycheck)
 
 (flycheck-define-checker smlsharp
-  "A SML# syntax checker using SML# compiler.
+  "A SML# syntax checker using SML# compiler in sml-mode.
+
+You need SML# compiler \"v3.4.0\". This checker calls SML# compiler with
+`-ftypecheck-only` option to check source code.
+
+This checker recognizes the following format strings of compiler.
+
+1. Error without position. For example, syntax error by not closed `let`.
+    - `none:~1.~1-~1.~1 Error: syntax error found at EOF`
+2. Error with position. For example, most sytax and type error.
+    - `myfile.sml:1.0-1.7 Error: syntax error: inserting COLON`
+    - `myfile.sml:6.8-6.9 Error:
+          (type inference 017) operator is not a function:
+          'FC::{int, int8, int16, int64,...}`
+3. Warning. For example, redundant or nonexhaustive match.
+    - `myfile.sml:10.0-10.10 Warning: match nonexhaustive
+             A  => ...`
+    - `myfile.sml:10.0-10.10 Warning:
+             match nonexhaustive
+                 A  => ...`
+    with long file name.
+
+Now, this checker only checks when the file is saved. That's because real-time
+flycheck creates a copy of source code with another file name. It makes
+difference name between sml file and smi, and makes checker complex.
 
 About SML#, see URL 'http://www.pllab.riec.tohoku.ac.jp/smlsharp/'."
   :command ("smlsharp" "-ftypecheck-only" source-original)
   :error-patterns
-   ; EOL errors do not have error line,
+   ; EOL errors do not have error position,
    ; like, "none:~1.~1-~1.~1 Error: syntax error found at EOF"
   ((error line-start "none:~1.~1-~1.~1 Error:" (+ (in " \t\n"))
           (message
            (and (+ not-newline) "\n"
                 (* line-start (+ blank) (+ not-newline) "\n"))))
-   ; Other errors have error line,
+   ; Other errors have error position,
    ;   like "file.sml:1.0-1.7 Error: syntax error: inserting COLON",
    ;   or like "file.sml:6.8-6.9 Error:
    ;              (type inference 017) operator is not a function:
